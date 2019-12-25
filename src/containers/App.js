cb8236608e57622d71fc3a673b41493bb58cd560
lebaluong33 <lebaluong33@gmail.com>
2019-12-25 23:01:04 +0700

    add Cockpit.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
+
 import './App.css';
 import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
     state = {
@@ -36,40 +38,18 @@ class App extends Component {
   }
   render(){
     let persons = null;
-    const styles = {
-      button : {
-        backgroundColor: "green",
-        color: "white",
-        font: "inherit",
-        border: "solid 1px blue",
-        padding: "8px",
-        cursor: "pointer",
-        fontSize: "25px",
-        width: "150px"
-      }
-    };
     if (this.state.showPerson) {
       persons = <Persons
                   clicked = {this.deletePersonHandler}
                   persons = {this.state.persons}
                   changed = {this.nameChangeHandler}/>;
-      styles.button.backgroundColor = "red";
     }
-    let classes = [];
-    if(this.state.persons.length <= 2) { 
-      classes.push('red')
-    };
-    if (this.state.persons.length <= 1) {
-      classes.push('bold');
-    };
     return(
     <div className = "App" >
-      <h1>Hi, I am React App</h1>
-      <p className={classes.join(' ')}>This is really working</p>
-      <button
-        style = {styles.button}
-        onClick={this.togglePerson}
-      >Switch</button>
+      <Cockpit
+        persons = {this.state.persons}
+        showPerson = {this.state.showPerson}
+        clicked = {this.togglePerson}/>
       {persons}
     </div>
   );
