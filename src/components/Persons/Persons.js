4e34f8808a587d2eea05e26afe2b59d2bba17eb7
lebaluong33 <lebaluong33@gmail.com>
2019-12-28 22:15:26 +0700

    change Persons.js to PureComponent, fix import App.css

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import Person from "./Person/Person";
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state) {
   //   console.log('[Persons.js] getDerivedStateFromProps');
   //   return state;
   // }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Persons.js] shouldComponentUpdate');
-    if(nextProps.persons !== this.props.persons)
-    return true;
-    else {
-      return false;
-    } 
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if(nextProps.persons !== this.props.persons)
+  //   return true;
+  //   else {
+  //     return false;
+  //   } 
+  // }
 
   getSnapshotBeforeUpdate(prevProps, PrevState){
     console.log('[Persons.js] getSnapshotBeforeUpdate');
